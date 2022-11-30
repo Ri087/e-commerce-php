f4f9dd6051a7c35cccf388e6a35bb43c9222f344
BVERNET <benjamin.vernet@ynov.com>
2022-11-30 16:06:20 +0100

    require files relocation

--- src/inc/bootstrap.php
+++ src/inc/bootstrap.php
@@ -1,13 +1,21 @@
 <?php
 define("PROJECT_ROOT_PATH", __DIR__ . "/../");
  
+// Temp file use to manage index of elements in URL
+require_once PROJECT_ROOT_PATH . "/inc/parameters.php";
+
 // include main configuration file
 require_once PROJECT_ROOT_PATH . "/inc/config.php";
-require_once PROJECT_ROOT_PATH . "/inc/parameters.php";
  
 // include the base controller file
 require_once PROJECT_ROOT_PATH . "/controller/api/baseController.php";
  
 // include the use model file
+require_once PROJECT_ROOT_PATH . "/model/database.php";
+
+// include all controller file (except the base)
+require_once PROJECT_ROOT_PATH . "/controller/api/userController.php";
+
+// include all the model file (except the database)
 require_once PROJECT_ROOT_PATH . "/model/userModel.php";
 ?>
--- src/model/userModel.php
+++ src/model/userModel.php
@@ -1,6 +1,4 @@
 <?php
-require_once PROJECT_ROOT_PATH . "/model/database.php";
- 
 class UserModel extends Database
 {
     public function getUsers()
