864d5bfa1effaee1da8603d9cc25e4e6b0a4e28c
BVERNET <benjamin.vernet@ynov.com>
2022-12-07 10:40:43 +0100

    Affichage by view contrller

--- index.php
+++ index.php
@@ -7,15 +7,11 @@ $base_url = "/e-commerce-php-les-bests-benjou-et-jeremoux";
 
 $router = new Router();
 
-$router->get($base_url . "", function () {
-    echo "Hello !";
-});
-$router->get($base_url . '/user/create', "User#createAction");
-$router->get($base_url . '/user/read', "User#readAction");
-$router->get($base_url . '/user/read/:id', "User#readAction");
-<<<<<<< HEAD
-=======
-$router->get($base_url . '/user/update', "User#updateAction");
->>>>>>> fc6788fc34d7a109d069100937fd8d3eab055895
+$router->get($base_url . '/profil', "View#profil");
+
+// $router->get($base_url . '/user/create', "User#createAction");
+// $router->get($base_url . '/user/read', "User#readAction");
+// $router->get($base_url . '/user/read/:id', "User#readAction");
+// $router->get($base_url . '/user/update', "User#updateAction");
 
 $router->run();
